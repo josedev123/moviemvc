cbf08af1cebdfb9cb63907327468801e121a3429
IMS <jose8055@gmail.com>
2019-01-17 16:46:14 -0500

    Added Option to Edit In DB

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -25,19 +25,31 @@ namespace moviemvc.Controllers
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipType;
-            var viewModel = new NewCustomerViewModel 
+            var viewModel = new CustomerFormViewModel 
             {
                 MembershipTypes = membershipTypes
             };
 
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
         }
 
         [HttpPost]
-        public ActionResult Create(Customer customer)
+        public ActionResult Save(Customer customer)
         {
+            if (customer.Id == 0)
             _context.Customers.Add(customer);
+            else
+            {
+                var CustomerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+
+               CustomerInDb.Name = customer.Name;
+               CustomerInDb.Birthdate = customer.Birthdate;
+               CustomerInDb.MembershipTypeId = customer.MembershipTypeId;
+               CustomerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+
+             }
             _context.SaveChanges();
+            
             return RedirectToAction("Index", "Customers");
         }
         public ViewResult Index()
@@ -56,7 +68,22 @@ namespace moviemvc.Controllers
 
             return View(customer);
         }
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
+            if (customer == null)
+            return NotFound();
+
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipType
+            };
+            return View("CustomerForm", viewModel);
+
+        }
 
     }
 }
---
+++ ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using moviemvc.Models;
+
+namespace moviemvc.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+    }
+} 
